f8713dcca34dbe6830c005a59a018ba8f1f65634
vlouf <v.louf@bom.gov.au>
2017-11-10 16:30:32 +1100

    Radar file name check extension

--- cpol_processing/processing_codes/radar_codes.py
+++ cpol_processing/processing_codes/radar_codes.py
@@ -548,7 +548,7 @@ def read_radar(radar_file_name):
     try:
         if ".h5" in radar_file_name:
             radar = pyart.aux_io.read_odim_h5(radar_file_name)
-        if ".hdf" in radar_file_name:
+        elif ".hdf" in radar_file_name:
             radar = pyart.aux_io.read_odim_h5(radar_file_name)
         else:
             radar = pyart.io.read(radar_file_name)
