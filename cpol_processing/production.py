9d3336e9673fc3d86126813b00f7661a2abe133e
vlouf <v.louf@bom.gov.au>
2017-11-10 15:06:59 +1100

    Correct attenuation from bringi.

--- cpol_processing/production.py
+++ cpol_processing/production.py
@@ -397,7 +397,7 @@ def production_line(radar_file_name, sound_dir, figure_path=None):
         pass
 
     # Correct Attenuation ZH
-    atten_spec, zh_corr = atten_codes.correct_attenuation_zh_pyart(radar)
+    atten_spec, zh_corr = atten_codes.correct_attenuation_zh(radar)
     radar.add_field('DBZ_CORR', zh_corr, replace_existing=True)
     radar.add_field('specific_attenuation_reflectivity', atten_spec, replace_existing=True)
     logger.info('Attenuation on reflectivity corrected.')
