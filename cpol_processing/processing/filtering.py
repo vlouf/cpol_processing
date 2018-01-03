62d99d7d121d6bb43eac2f26d9e4d1835b1b68b3
vlouf <v.louf@bom.gov.au>
2018-01-03 17:17:39 +1100

    Nonetype error from seapol

--- cpol_processing/processing/filtering.py
+++ cpol_processing/processing/filtering.py
@@ -94,11 +94,14 @@ def do_gatefilter(radar, refl_name='DBZ', phidp_name="PHIDP", rhohv_name='RHOHV_
         gf.include_above(rhohv_name, 0.8)
     else:
         # Using PHIDP texture for filtering.
-        tphi = pyart.retrieve.calculate_velocity_texture(radar, vel_field=phidp_name, nyq=90)
-        radar.add_field("TPHI", tphi, replace_existing=True)
-        gf.exclude_above("TPHI", 30)
-        gf.exclude_below(rhohv_name, 0.5)
-        radar.fields.pop('TPHI')
+        try:
+            tphi = pyart.retrieve.calculate_velocity_texture(radar, vel_field=phidp_name, nyq=90)
+            radar.add_field("TPHI", tphi, replace_existing=True)
+            gf.exclude_above("TPHI", 30)
+            gf.exclude_below(rhohv_name, 0.5)
+            radar.fields.pop('TPHI')
+        except Exception:
+            pass
 
     zdr = radar.fields[zdr_name]['data']
     dbz = radar.fields[refl_name]['data']
