5269f50fc36045c137dc3032829f7329d7b2253f
vlouf <v.louf@bom.gov.au>
2018-04-12 14:17:40 +1000

    Reinstate smooth PHIDP for giangrande technique.

--- cpol_processing/production.py
+++ cpol_processing/production.py
@@ -458,7 +458,11 @@ def production_line(radar_file_name, sound_dir, figure_path=None):
     radar.fields['KDP_BRINGI']['long_name'] = "bringi_corrected_specific_differential_phase"
     logger.info('KDP/PHIDP Bringi estimated.')
 
-    phidp_gg, kdp_gg = phase.phidp_giangrande(radar, gatefilter, phidp_field='PHI_UNF', rhv_field='RHOHV_CORR')
+    if gatefilter.gate_included.sum() / gatefilter.gate_excluded.sum() < 0.3:
+        # small scene/not enough data. Using smoothed PHIDP.
+        phidp_gg, kdp_gg = phase.phidp_giangrande(radar, gatefilter, phidp_field='PHIDP_BRINGI', rhv_field='RHOHV_CORR')
+    else:
+        phidp_gg, kdp_gg = phase.phidp_giangrande(radar, gatefilter, phidp_field='PHI_UNF', rhv_field='RHOHV_CORR')
     if half_phi:
         phidp_gg['data'] /= 2
         kdp_gg['data'] /= 2
