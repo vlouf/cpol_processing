1c0e112dfa8339076a3fae0d2e14f3460780ee85
Valentin Louf <v.louf@bom.gov.au>
2018-12-13 13:20:08 -0500

    MODIF: Put first phidp gates to zero.

--- cpol_processing/processing/phase.py
+++ cpol_processing/processing/phase.py
@@ -159,6 +159,7 @@ def phidp_giangrande(radar, gatefilter, refl_field='DBZ', ncp_field='NCP',
     if half_phi:
         unfphi['data'] *= 2
 
+    unfphi['data'][:, :50] = 0
     radar.fields[phidp_field]['data'] = unfphi['data']
     # Pyart version 1.10.
     phidp_gg, kdp_gg = pyart.correct.phase_proc_lp_gf(radar,
