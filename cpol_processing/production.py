39c828c469cb2e0e4eb6bf4c1583ea455df809aa
vlouf <v.louf@bom.gov.au>
2018-12-20 15:07:23 +1100

    MODIF: print function

--- cpol_processing/production.py
+++ cpol_processing/production.py
@@ -557,7 +557,8 @@ def production_line(radar_file_name, sound_dir, figure_path=None, is_cpol=True, 
     # zdr_corr = attenuation.correct_attenuation_zdr(radar, phidp_name=phidp_field_name, zdr_name='ZDR_CORR')
     zdr_corr_atten = radar.fields['ZDR_CORR']['data'].copy() + 0.016 * radar.fields[phidp_field_name]['data'].copy()
     radar.add_field_like('ZDR', 'ZDR_CORR_ATTEN', zdr_corr_atten)
-    print('ZDR attenuation corrected. Mean difference is: {}'.format(np.nanmean(zdr_corr_atten['data'] - radar.fields['ZDR']['data'])))
+    print('ZDR attenuation corrected. Mean difference is: {}'.format(np.nanmean(zdr_corr_atten['data'].flatten() -
+                                                                                radar.fields['ZDR']['data'].flatten())))
     # logger.info('Attenuation on ZDR corrected.')
 
     # Hydrometeors classification
