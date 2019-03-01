d18decd209af2fbc193c99c96b87010a4835f619
vlouf <v.louf@bom.gov.au>
2019-03-01 12:18:30 +1100

    Delete temp fields

--- cpol_processing/production.py
+++ cpol_processing/production.py
@@ -631,5 +631,9 @@ def production_line(radar_file_name, sound_dir, figure_path=None, is_cpol=True):
     goodkeys = ["radar_echo_classification", "D0", "NW", "velocity", "total_power",
                 "reflectivity", "cross_correlation_ratio", "corrected_differential_reflectivity",
                 "corrected_differential_phase", "corrected_specific_differential_phase", "spectrum_width"]
+    # Delete working variables.
+    for k in radar.fields.keys():
+        if k not in goodkeys:
+            radar.fields.pop(k)
 
     return radar
