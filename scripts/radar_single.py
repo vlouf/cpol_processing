46db3fdf94230d8c8221fda869b395cd399e83fa
Valentin Louf <valentin.louf@bom.gov.au>
2020-06-27 16:10:14 +1000

    MODIF: rem obsolete comments

--- scripts/radar_single.py
+++ scripts/radar_single.py
@@ -4,9 +4,9 @@ dealiasing, unfolding, hydrometeors calculation, rainfall rate estimation.
 Tested on CPOL.
 
 @title: cpol_processing
-@author: Valentin Louf <valentin.louf@monash.edu>
-@institution: Monash University
-@date: 13/03/2019
+@author: Valentin Louf <valentin.louf@bom.gov.au>
+@institution: Monash University and Australian Bureau of Meteorology
+@date: 27/06/2020
 @version: 2
 
 .. autosummary::
@@ -19,8 +19,8 @@ import os
 import argparse
 import warnings
 
-# Other Libraries
 import crayons
+import cpol_processing
 
 
 def main():
@@ -41,60 +41,50 @@ def main():
     print("\n" + "#" * 79 + "\n")
 
     with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        import cpol_processing
+        warnings.simplefilter("ignore")
         cpol_processing.process_and_save(INFILE, OUTPATH, SOUND_DIR, use_unravel=USE_UNRAVEL)
 
     print(crayons.green("Process completed."))
-
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Global variables definition and logging file initialisation.
     """
-    # Input directory for Radiosoundings (use my other script, named caprica to
-    # download and format these datas).
-    # INPATH = "/g/data/hj10/cpol_level_1a/ppi/"
-    # OUTPATH = "/g/data/hj10/cpol_level_1b/"
-
-
     # Parse arguments
     parser_description = """Raw radar PPIs processing. It provides Quality
 control, filtering, attenuation correction, dealiasing, unfolding, hydrometeors
 calculation, and rainfall rate estimation."""
     parser = argparse.ArgumentParser(description=parser_description)
     parser.add_argument(
-        '-i',
-        '--input',
-        dest='infile',
-        type=str,
-        help='Input file',
-        required=True)
+        "-i", "--input", dest="infile", type=str, help="Input file", required=True
+    )
     parser.add_argument(
-        '-o',
-        '--output',
-        dest='outdir',
+        "-o",
+        "--output",
+        dest="outdir",
         type=str,
-        help='Output directory.',
-        required=True)
+        help="Output directory.",
+        required=True,
+    )
     parser.add_argument(
-        '-r',
-        '--radiosounds',
-        dest='rs_dir',
+        "-r",
+        "--radiosounds",
+        dest="rs_dir",
         type=str,
-        help='Radiosoundings directory.',
-        default="/g/data/kl02/vhl548/darwin_ancillary/DARWIN_radiosonde")
+        help="Radiosoundings directory.",
+        default="/g/data/kl02/vhl548/darwin_ancillary/DARWIN_radiosonde",
+    )
 
-    parser.add_argument('--unravel', dest='unravel', action='store_true')
-    parser.add_argument('--no-unravel', dest='unravel', action='store_false')
+    parser.add_argument("--unravel", dest="unravel", action="store_true")
+    parser.add_argument("--no-unravel", dest="unravel", action="store_false")
     parser.set_defaults(unravel=True)
 
     args = parser.parse_args()
     INFILE = args.infile
     OUTPATH = args.outdir
-    SOUND_DIR =  args.rs_dir
+    SOUND_DIR = args.rs_dir
     USE_UNRAVEL = args.unravel
 
     if not os.path.isfile(INFILE):
