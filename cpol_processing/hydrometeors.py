b716b4e2908fc76d3fad0256edd2d29a69020881
Valentin Louf <valentin.louf@bom.gov.au>
2020-08-26 17:02:40 +1000

    MODIF: pep8

--- cpol_processing/hydrometeors.py
+++ cpol_processing/hydrometeors.py
@@ -23,7 +23,7 @@ import numpy as np
 from csu_radartools import csu_liquid_ice_mass, csu_fhc, csu_blended_rain, csu_dsd
 
 
-def dsd_retrieval(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR'):
+def dsd_retrieval(radar, gatefilter, kdp_name, zdr_name, refl_name="DBZ_CORR"):
     """
     Compute the DSD retrieval using the csu library.
 
@@ -45,14 +45,14 @@ def dsd_retrieval(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR'):
     d0_dict: dict
         Median Volume Diameter.
     """
-    dbz = radar.fields[refl_name]['data'].copy().filled(np.NaN)
-    zdr = radar.fields[zdr_name]['data'].copy()
+    dbz = radar.fields[refl_name]["data"].copy().filled(np.NaN)
+    zdr = radar.fields[zdr_name]["data"].copy()
     try:
-        kdp = radar.fields[kdp_name]['data'].copy().filled(np.NaN)
+        kdp = radar.fields[kdp_name]["data"].copy().filled(np.NaN)
     except AttributeError:
-        kdp = radar.fields[kdp_name]['data'].copy()
+        kdp = radar.fields[kdp_name]["data"].copy()
 
-    d0, Nw, mu = csu_dsd.calc_dsd(dz=dbz, zdr=zdr, kdp=kdp, band='C')
+    d0, Nw, mu = csu_dsd.calc_dsd(dz=dbz, zdr=zdr, kdp=kdp, band="C")
 
     Nw = np.log10(Nw)
     Nw[gatefilter.gate_excluded] = np.NaN
@@ -63,27 +63,37 @@ def dsd_retrieval(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR'):
     d0 = np.ma.masked_invalid(d0).astype(np.float32)
     np.ma.set_fill_value(d0, np.NaN)
 
-    nw_dict = {'data': Nw,
-               'long_name': 'normalized_intercept_parameter',
-               'units': ' ',
-               '_FillValue': np.NaN,
-               '_Least_significant_digit': 2,
-               'reference': "doi:10.1175/2009JTECHA1258.1"}
+    nw_dict = {
+        "data": Nw,
+        "long_name": "normalized_intercept_parameter",
+        "units": " ",
+        "_FillValue": np.NaN,
+        "_Least_significant_digit": 2,
+        "reference": "doi:10.1175/2009JTECHA1258.1",
+    }
 
-    d0_dict = {'data': d0,
-               'units': 'mm',
-               'long_name': 'median_volume_diameter',
-               '_FillValue': np.NaN,
-               '_Least_significant_digit': 2,
-               'reference': "doi:10.1175/2009JTECHA1258.1"}
+    d0_dict = {
+        "data": d0,
+        "units": "mm",
+        "long_name": "median_volume_diameter",
+        "_FillValue": np.NaN,
+        "_Least_significant_digit": 2,
+        "reference": "doi:10.1175/2009JTECHA1258.1",
+    }
 
     return nw_dict, d0_dict
 
 
-def hydrometeor_classification(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR',
-                               rhohv_name='RHOHV_CORR',
-                               temperature_name='temperature',
-                               height_name='height'):
+def hydrometeor_classification(
+    radar,
+    gatefilter,
+    kdp_name,
+    zdr_name,
+    refl_name="DBZ_CORR",
+    rhohv_name="RHOHV_CORR",
+    temperature_name="temperature",
+    height_name="height",
+):
     """
     Compute hydrometeo classification.
 
@@ -109,39 +119,48 @@ def hydrometeor_classification(radar, gatefilter, kdp_name, zdr_name, refl_name=
     hydro_meta: dict
         Hydrometeor classification.
     """
-    refl = radar.fields[refl_name]['data'].copy().filled(np.NaN)
-    zdr = radar.fields[zdr_name]['data'].copy().filled(np.NaN)
+    refl = radar.fields[refl_name]["data"].copy().filled(np.NaN)
+    zdr = radar.fields[zdr_name]["data"].copy().filled(np.NaN)
     try:
-        kdp = radar.fields[kdp_name]['data'].copy().filled(np.NaN)
+        kdp = radar.fields[kdp_name]["data"].copy().filled(np.NaN)
     except AttributeError:
-        kdp = radar.fields[kdp_name]['data'].copy()
-    rhohv = radar.fields[rhohv_name]['data']
+        kdp = radar.fields[kdp_name]["data"].copy()
+    rhohv = radar.fields[rhohv_name]["data"]
     try:
-        radar_T = radar.fields[temperature_name]['data']
+        radar_T = radar.fields[temperature_name]["data"]
         use_temperature = True
     except Exception:
         use_temperature = False
 
     if use_temperature:
-        scores = csu_fhc.csu_fhc_summer(dz=refl, zdr=zdr, rho=rhohv, kdp=kdp, use_temp=True, band='C', T=radar_T)
+        scores = csu_fhc.csu_fhc_summer(dz=refl, zdr=zdr, rho=rhohv, kdp=kdp, use_temp=True, band="C", T=radar_T)
     else:
-        scores = csu_fhc.csu_fhc_summer(dz=refl, zdr=zdr, rho=rhohv, kdp=kdp, use_temp=False, band='C')
+        scores = csu_fhc.csu_fhc_summer(dz=refl, zdr=zdr, rho=rhohv, kdp=kdp, use_temp=False, band="C")
 
     hydro = np.argmax(scores, axis=0) + 1
     hydro[gatefilter.gate_excluded] = 0
     hydro_data = np.ma.masked_equal(hydro.astype(np.int16), 0)
 
-    the_comments = "1: Drizzle; 2: Rain; 3: Ice Crystals; 4: Aggregates; " +\
-                   "5: Wet Snow; 6: Vertical Ice; 7: LD Graupel; 8: HD Graupel; 9: Hail; 10: Big Drops"
+    the_comments = (
+        "1: Drizzle; 2: Rain; 3: Ice Crystals; 4: Aggregates; "
+        + "5: Wet Snow; 6: Vertical Ice; 7: LD Graupel; 8: HD Graupel; 9: Hail; 10: Big Drops"
+    )
 
-    hydro_meta = {'data': hydro_data, 'units': ' ', 'long_name': 'Hydrometeor classification', '_FillValue': np.int16(0),
-                  'standard_name': 'Hydrometeor_ID', 'comments': the_comments}
+    hydro_meta = {
+        "data": hydro_data,
+        "units": " ",
+        "long_name": "Hydrometeor classification",
+        "_FillValue": np.int16(0),
+        "standard_name": "Hydrometeor_ID",
+        "comments": the_comments,
+    }
 
     return hydro_meta
 
 
-def merhala_class_convstrat(radar, dbz_name="DBZ_CORR", rain_name="radar_estimated_rain_rate",
-                            d0_name="D0", nw_name="NW"):
+def merhala_class_convstrat(
+    radar, dbz_name="DBZ_CORR", rain_name="radar_estimated_rain_rate", d0_name="D0", nw_name="NW"
+):
     """
     Merhala Thurai's has a criteria for classifying rain either Stratiform
     Convective or Mixed, based on the D-Zero value and the log10(Nw) value.
@@ -161,9 +180,9 @@ def merhala_class_convstrat(radar, dbz_name="DBZ_CORR", rain_name="radar_estimat
         Merhala Thurai classification.
     """
     # Extracting data.
-    d0 = radar.fields[d0_name]['data']
-    nw = radar.fields[nw_name]['data']
-    dbz = radar.fields[dbz_name]['data']
+    d0 = radar.fields[d0_name]["data"]
+    nw = radar.fields[nw_name]["data"]
+    dbz = radar.fields[dbz_name]["data"]
 
     classification = np.zeros(dbz.shape, dtype=np.int16)
 
@@ -184,18 +203,28 @@ def merhala_class_convstrat(radar, dbz_name="DBZ_CORR", rain_name="radar_estimat
     classification = np.ma.masked_where(~pos0 | ~pos1 | dbz.mask, classification)
 
     # Generate metada.
-    class_meta = {'data': classification,
-                  'long_name': 'thurai_echo_classification',
-                  'valid_min': 0,
-                  'valid_max': 3,
-                  'comment_1': 'Convective-stratiform echo classification based on Merhala Thurai',
-                  'comment_2': '0 = Undefined, 1 = Stratiform, 2 = Convective, 3 = Mixed'}
+    class_meta = {
+        "data": classification,
+        "long_name": "thurai_echo_classification",
+        "valid_min": 0,
+        "valid_max": 3,
+        "comment_1": "Convective-stratiform echo classification based on Merhala Thurai",
+        "comment_2": "0 = Undefined, 1 = Stratiform, 2 = Convective, 3 = Mixed",
+    }
 
     return class_meta
 
 
-def rainfall_rate(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR',
-                  hydro_name='radar_echo_classification', temperature_name='temperature', band="C"):
+def rainfall_rate(
+    radar,
+    gatefilter,
+    kdp_name,
+    zdr_name,
+    refl_name="DBZ_CORR",
+    hydro_name="radar_echo_classification",
+    temperature_name="temperature",
+    band="C",
+):
     """
     Rainfall rate algorithm from csu_radartools.
 
@@ -219,30 +248,32 @@ def rainfall_rate(radar, gatefilter, kdp_name, zdr_name, refl_name='DBZ_CORR',
     rainrate: dict
         Rainfall rate.
     """
-    dbz = radar.fields[refl_name]['data'].filled(np.NaN)
-    zdr = radar.fields[zdr_name]['data'].filled(np.NaN)
-    fhc = radar.fields[hydro_name]['data']
+    dbz = radar.fields[refl_name]["data"].filled(np.NaN)
+    zdr = radar.fields[zdr_name]["data"].filled(np.NaN)
+    fhc = radar.fields[hydro_name]["data"]
     try:
-        kdp = radar.fields[kdp_name]['data'].filled(np.NaN)
+        kdp = radar.fields[kdp_name]["data"].filled(np.NaN)
     except AttributeError:
-        kdp = radar.fields[kdp_name]['data']
+        kdp = radar.fields[kdp_name]["data"]
 
     rain, _ = csu_blended_rain.calc_blended_rain_tropical(dz=dbz, zdr=zdr, kdp=kdp, fhc=fhc, band=band)
 
     rain[(gatefilter.gate_excluded) | np.isnan(rain) | (rain < 0)] = 0
 
     try:
-        temp = radar.fields[temperature_name]['data']
+        temp = radar.fields[temperature_name]["data"]
         rain[temp < 0] = 0
     except Exception:
         pass
 
-    rainrate = {"long_name": 'Blended Rainfall Rate',
-                "units": "mm h-1",
-                "standard_name": "rainfall_rate",
-                '_Least_significant_digit': 2,
-                '_FillValue': np.NaN,
-                "description": "Rainfall rate algorithm based on Thompson et al. 2016.",
-                "data": rain.astype(np.float32)}
+    rainrate = {
+        "long_name": "Blended Rainfall Rate",
+        "units": "mm h-1",
+        "standard_name": "rainfall_rate",
+        "_Least_significant_digit": 2,
+        "_FillValue": np.NaN,
+        "description": "Rainfall rate algorithm based on Thompson et al. 2016.",
+        "data": rain.astype(np.float32),
+    }
 
     return rainrate
